6031ab3e4daaa4751dc51b4dc1eb57c6be92b0b4
Doris Meiers <dogomezmeiers@gmail.com>
2020-10-10 16:13:33 -0500

    adding changes in PyBank Homework(comments)

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,17 @@
 import os
 import csv
 
-total_months = 0
-total_profits = 0
-total_average_change = 0
-
-# Dictionary created to store Date as a Key and the the amount change of each month
-myBudgetData = {}
- 
 my_folder = os.path.dirname(os.path.abspath('budget_data.csv'))
 csvpath = os.path.join(my_folder,"Resources",'budget_data.csv')
+
+# created to store the total number of months
+total_months = 0
+# created to store the total amount of "Profit/Losses"
+total_profits = 0
+# created to store the average of the changes
+total_average_change = 0
+# Dictionary created to store Date as a Key and the the amount change of each month
+myBudgetData = {}
 
 with open(csvpath) as csvfile:
 
@@ -22,7 +24,7 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         # count total number of months in the dataset 
         total_months += 1
-        # sum otal amount of "Profit/Losses" 
+        # sum total amount of "Profit/Losses" 
         total_profits += int(row[1])
        
         # Calculate and save into the dictionay the 'amount change' from the second date
@@ -30,7 +32,7 @@ with open(csvpath) as csvfile:
            # save the date(row[0]) as key and the difference between current amount and previous amount as value
            myBudgetData[row[0]]= int(row[1]) - v_old_profit
     
-           # save the previous amount
+        # save the previous amount
         v_old_profit = int(row[1])
 
 # Function that returns the average change (sum values from the dictionay divided by the total months less the first month)
@@ -41,14 +43,14 @@ def average_change():
 def greatest_increase():
     return(str(max(myBudgetData, key=myBudgetData.get)) + ' ($' + str( max(myBudgetData.values()))+')')
 
-# Function the greatest decrease in losses (date and amount)
+# Function that returns the greatest decrease in losses (date and amount)
 def greatest_decrease():
     return(str(min(myBudgetData, key=myBudgetData.get)) + ' ($' + str( min(myBudgetData.values()))+')')
 
-# Funtion that print to the terminal and export the result to a text file
+# Function that prints to the terminal and exports the result to a text file
 def print_ouputs():
 
-    #print to the terminal
+    # print to the terminal
     print('-------------------------------------')
     print('Financial Analysis')
     print('-------------------------------------')
@@ -59,7 +61,7 @@ def print_ouputs():
     print('Greatest Increase in Profits: ',greatest_increase())
     print('Greatest Decrease in Profits: ',greatest_decrease())
 
-    #export the text file
+    # export the text file
     analysis_output = os.path.join("analysis", "financial_analysis.txt")
     with open(analysis_output, 'w') as analysis_txt:
          analysis_txt.write('Election Results:\n')
